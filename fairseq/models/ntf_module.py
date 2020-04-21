ebda1b9676a413166d8941de5052435eb2b31a73
Rivindu <rivindu.w@gmail.com>
2020-04-22 10:15:15 +1200

    log flow residual

--- fairseq/models/ntf_module.py
+++ fairseq/models/ntf_module.py
@@ -104,6 +104,8 @@ class NTF_Module(nn.Module):
                 wandb.log({"current_flows_2": wandb.Histogram(self.current_flows[:, 1].cpu().detach().numpy())})
                 wandb.log({"current_flows_3": wandb.Histogram(self.current_flows[:, 2].cpu().detach().numpy())})
                 wandb.log({"current_flows_4": wandb.Histogram(self.current_flows[:, 3].cpu().detach().numpy())})
+
+                wandb.log({"flow_residual": wandb.Histogram(self.flow_residual.cpu().detach().numpy())})
                 
                 wandb.log({"current_flows_1_to_2": wandb.Histogram(self.current_flows[:, 1].cpu().detach().numpy()-self.current_flows[:, 0].cpu().detach().numpy())})
                 wandb.log({"current_flows_2_to_3": wandb.Histogram(self.current_flows[:, 2].cpu().detach().numpy()-self.current_flows[:, 1].cpu().detach().numpy())})
@@ -133,7 +135,7 @@ class NTF_Module(nn.Module):
               + self.epsv
 
     def future_rho(self):
-        flow_residual = (self.prev_flows - self.current_flows + self.current_onramp - self.current_offramp)
+        self.flow_residual = (self.prev_flows - self.current_flows + self.current_onramp - self.current_offramp)
         #flow_residual = torch.clamp(flow_residual, min=0, max=10000)
         return self.current_densities + \
             torch.mul(torch.div(self.t_var,torch.mul(self.cap_delta,self.lambda_var)),\
