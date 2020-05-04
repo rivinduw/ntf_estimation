9a5534bb65a9aeae4fa8dd5211e21f6642f4d586
Rivindu <rivindu.w@gmail.com>
2020-05-04 22:10:01 +1200

    small bugs

--- fairseq/data/traffic_dataset.py
+++ fairseq/data/traffic_dataset.py
@@ -146,9 +146,9 @@ class TrafficDataset(FairseqDataset):
         all_dates = pd.to_datetime(self.all_data.index)
         all_context = pd.DataFrame(all_dates.dayofweek.values/6.0,columns=['dow'])
         all_context['tod'] = (all_dates.hour * 60. + all_dates.minute) / 1440.
-        all_context.index = data.index
+        all_context.index = self.all_data.index
         input_cols_to_include = np.arange(len(all_active_vars))[all_active_vars]
-        self.all_input_data = self.all_input_data/self.max_vals
+        self.all_input_data = self.all_data/self.max_vals
         self.all_input_data = self.all_input_data.iloc[:,input_cols_to_include]
         self.all_input_data = pd.concat([self.all_input_data,all_context],axis=1)
         
