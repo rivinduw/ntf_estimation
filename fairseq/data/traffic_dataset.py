98361a0e76e3ea65a326b55bcc4be2913e8cbdb1
Rivindu <rivindu.w@gmail.com>
2020-05-14 20:30:36 +1200

    dataset and viz fixes

--- fairseq/data/traffic_dataset.py
+++ fairseq/data/traffic_dataset.py
@@ -143,14 +143,15 @@ class TrafficDataset(FairseqDataset):
 
 
         #add context
-        all_dates = pd.to_datetime(self.all_data.index)
-        all_context = pd.DataFrame(all_dates.dayofweek.values/6.0,columns=['dow'])
-        all_context['tod'] = (all_dates.hour * 60. + all_dates.minute) / 1440.
-        all_context.index = self.all_data.index
-        input_cols_to_include = np.arange(len(all_active_vars))[all_active_vars]
-        self.all_input_data = self.all_data/self.max_vals
-        self.all_input_data = self.all_input_data.iloc[:,input_cols_to_include]
-        self.all_input_data = pd.concat([self.all_input_data,all_context],axis=1)
+        # all_dates = pd.to_datetime(self.all_data.index)
+        # all_context = pd.DataFrame(all_dates.dayofweek.values/6.0,columns=['dow'])
+        # all_context['tod'] = (all_dates.hour * 60. + all_dates.minute) / 1440.
+        # all_context.index = self.all_data.index
+        # input_cols_to_include = np.arange(len(all_active_vars))[all_active_vars]
+        # self.all_input_data = self.all_data/self.max_vals
+        # self.all_input_data = self.all_input_data.iloc[:,input_cols_to_include]
+        # self.all_input_data = pd.concat([self.all_input_data,all_context],axis=1)
+        self.all_input_data = self.all_data
         
         self.shuffle = shuffle
     
@@ -191,9 +192,10 @@ class TrafficDataset(FairseqDataset):
 
         one_label[:,::self.variables_per_segment] = self.mainlines_to_include_in_output * one_label[:,::self.variables_per_segment]
         one_label[:,1::self.variables_per_segment] = self.mainlines_to_include_in_output * one_label[:,1::self.variables_per_segment]
+        one_label[:,2::self.variables_per_segment] = self.mainlines_to_include_in_output * one_label[:,1::self.variables_per_segment]
         one_label[one_label==0] = NEG
-        one_label[:,2::self.variables_per_segment] = one_label[:,2::self.variables_per_segment] #+ 1e-3
-        one_label[:,3::self.variables_per_segment] = one_label[:,3::self.variables_per_segment] #+ 1e-3
+        one_label[:,3::self.variables_per_segment] = one_label[:,2::self.variables_per_segment] #+ 1e-3
+        one_label[:,4::self.variables_per_segment] = one_label[:,3::self.variables_per_segment] #+ 1e-3
         
         if self.scale_output:
           one_label = one_label/self.max_vals
