c21a2c8f975651029b8a9607d5fcdd3a74e00c72
Rivindu <rivindu.w@gmail.com>
2020-05-18 13:36:22 +1200

    no flow input wierd

--- fairseq/data/traffic_dataset.py
+++ fairseq/data/traffic_dataset.py
@@ -146,30 +146,31 @@ class TrafficDataset(FairseqDataset):
         # self.all_data      = self.all_data_5min#self.all_data.iloc[10:,:]
 
         ##print 
-        all_flows = self.all_data.iloc[:,::self.variables_per_segment]
-        all_flows = all_flows[all_flows>0]
-        self.mean_flow = all_flows.mean().mean()
-        self.std_flow = all_flows.std().mean()
+        print(self.all_data.columns)
+        # all_flows = self.all_data.iloc[:,::self.variables_per_segment]
+        # all_flows = all_flows[all_flows>0]
+        # self.mean_flow = all_flows.mean().mean()
+        # self.std_flow = all_flows.std().mean()
         
-        all_densities = self.all_data.iloc[:,1::self.variables_per_segment]
+        all_densities = self.all_data.iloc[:,0::self.variables_per_segment]
         all_densities = all_densities[all_densities>0]
         self.mean_density = all_densities.mean().mean()
         self.std_density = all_densities.std().mean()
 
-        all_speeds = self.all_data.iloc[:,2::self.variables_per_segment]
+        all_speeds = self.all_data.iloc[:,1::self.variables_per_segment]
         all_speeds = all_speeds[all_speeds>0]
         self.mean_speed = all_speeds.mean().mean()
         self.std_speed = all_speeds.std().mean()
 
-        all_onramps = self.all_data.iloc[:,3::self.variables_per_segment]
+        all_onramps = self.all_data.iloc[:,2::self.variables_per_segment]
         all_onramps = all_onramps[all_onramps>0]
         self.mean_onramp = all_onramps.mean().mean()
         self.std_onramp = all_onramps.std().mean()
 
-        # all_offramps = self.all_data.iloc[:,4::self.variables_per_segment]
-        # all_offramps = all_offramps[all_offramps>0]
-        # self.mean_offramp = all_offramps.mean().mean()
-        # self.std_offramp = all_offramps.std().mean()
+        all_offramps = self.all_data.iloc[:,3::self.variables_per_segment]
+        all_offramps = all_offramps[all_offramps>0]
+        self.mean_offramp = all_offramps.mean().mean()
+        self.std_offramp = all_offramps.std().mean()
 
         # self.all_means = [self.mean_flow,self.mean_density,self.mean_speed,self.mean_onramp,self.mean_offramp]*self.num_segments 
         # self.all_stds = [self.std_flow,self.std_density,self.std_speed,self.std_onramp,self.std_offramp]*self.num_segments 
@@ -177,11 +178,11 @@ class TrafficDataset(FairseqDataset):
         # print("means:",self.mean_flow,self.mean_density,self.mean_speed,self.mean_onramp,self.mean_offramp)
         # print("stds:",self.std_flow,self.std_density,self.std_speed,self.std_onramp,self.std_offramp)
 
-        self.all_means = [self.mean_flow,self.mean_density,self.mean_speed,self.mean_onramp]*self.num_segments 
-        self.all_stds = [self.std_flow,self.std_density,self.std_speed,self.std_onramp]*self.num_segments 
+        self.all_means = [self.mean_density,self.mean_speed,self.mean_onramp,self.mean_offramp]*self.num_segments 
+        self.all_stds = [self.std_density,self.std_speed,self.std_onramp,self.std_offramp]*self.num_segments 
 
-        print("means:",self.mean_flow,self.mean_density,self.mean_speed,self.mean_onramp)
-        print("stds:",self.std_flow,self.std_density,self.std_speed,self.std_onramp)
+        print("means:",self.mean_density,self.mean_speed,self.mean_onramp,self.mean_offramp)
+        print("stds:",self.std_density,self.std_speed,self.std_onramp,self.std_offramp)
 
 
         # add context
